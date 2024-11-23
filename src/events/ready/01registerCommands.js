3469216c5f8162c70f56d372298eb0861c557786
IonutRicardo <ionutricardointzz055@gmail.com>
2024-11-23 16:23:56 +0200

    -

---
+++ src/events/ready/01registerCommands.js
@@ -0,0 +1,54 @@
+const { serverId } = require('../../../config.json');
+const getApplicationCommands = require('../../utils/getApplicationCommands');
+const areCommandsDifferent = require('../../utils/areCommandsDifferent');
+const getLocalCommands = require('../../utils/getLocalCommands');
+
+module.exports = async (client) => {
+	try {
+		const localCommands = getLocalCommands();
+		const applicationCommands = await getApplicationCommands(client, serverId);
+
+		for (const localCommand of localCommands) {
+			const { name, description, options } = localCommand;
+
+			const existingCommand = await applicationCommands.cache.find(
+				(cmd) => cmd.name === name
+			);
+
+			if (existingCommand) {
+				if (localCommand.deleted) {
+					await applicationCommands.delete(existingCommand.id);
+
+					console.log(`🗑️ Deleted command "${name}".`);
+					continue;
+				}
+
+				if (areCommandsDifferent(existingCommand, localCommand)) {
+					await applicationCommands.edit(existingCommand.id, {
+						description,
+						options,
+					});
+
+					console.log(`🔃 Edited command "${name}".`);
+				}
+			} else {
+				if (localCommand.deleted) {
+					console.log(
+						`⏭️ Skipping registering command "${name}" as it's set to delete.`
+					);
+					continue;
+				}
+
+				await applicationCommands.create({
+					name,
+					description,
+					options,
+				});
+
+				console.log(`✅ Registered command "${name}".`);
+			}
+		}
+	} catch (error) {
+		console.log(`❌ There was an error: ${error}`);
+	}
+};
---
+++ src/events/ready/consoleLog.js
@@ -0,0 +1,3 @@
+module.exports = (client) => {
+	console.log(`✅ [${client.user.tag}] [${client.user.id}] (ONLINE)`);
+};
